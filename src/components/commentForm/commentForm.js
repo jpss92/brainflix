6d932ad9e120950fca3a47721af04d0bd2f2aab7
Justin Swallow <132158985+jpss92@users.noreply.github.com>
2023-09-02 11:02:42 -0700

    adjusted videoSection

--- src/components/commentForm/commentForm.js
+++ src/components/commentForm/commentForm.js
@@ -3,11 +3,12 @@ import '../commentForm/commentForm.scss';
 import Button from '../button/button.js';
 import addComment from '../../assets/Icons/add_comment.svg';
 import profileColor from '../../assets/Images/AFAFAF.png.png';
+import profileImg from '../../assets/Images/Mohan-muruge.jpg';
 
 function CommentForm() {
     return (
         <div className="comment__form">
-            <Avatar className="nav__avatar" src={profileColor} id='comment__avatar'/>
+            <Avatar className="nav__avatar" src={profileImg} id='comment__avatar'/>
         <form className="comment__form--data" >
             <label>JOIN THE CONVERSATION</label>
             <textarea className="comment__form--input" placeholder="Add a new comment"></textarea>
